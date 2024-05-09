d4149564251236700c8647069ab1deb4daa090c1
SledDev <sleddev@asd.dog>
2024-05-09 04:53:15 +0200

    updated tray build script

--- tray_windows.bat
+++ tray_windows.bat
@@ -1,12 +1,12 @@
 taskkill /f /fi "windowtitle eq TGStickers tray"
 mkdir tray\windows\temp
-mkdir build\windows\runner\Debug
-mkdir build\windows\runner\Release
+mkdir build\windows\x64\runner\Debug
+mkdir build\windows\x64\runner\Release
 windres tray\windows\icon.rc tray\windows\temp\icon.o
-g++ -static tray\windows\systemtray.cpp tray\windows\temp\icon.o -o build\windows\runner\Debug\tgs-tray.exe -lgdi32 -mwindows
-copy build\windows\runner\Debug\tgs-tray.exe build\windows\runner\Release\tgs-tray.exe
-copy tray\windows\vcruntime140_1.dll build\windows\runner\Debug\vcruntime140_1.dll
-copy build\windows\runner\Debug\vcruntime140_1.dll build\windows\runner\Release\vcruntime140_1.dll
+g++ -static tray\windows\systemtray.cpp tray\windows\temp\icon.o -o build\windows\x64\runner\Debug\tgs-tray.exe -lgdi32 -mwindows
+copy build\windows\x64\runner\Debug\tgs-tray.exe build\windows\x64\runner\Release\tgs-tray.exe
+copy tray\windows\vcruntime140_1.dll build\windows\x64\runner\Debug\vcruntime140_1.dll
+copy build\windows\runner\Debug\vcruntime140_1.dll build\windows\x64\runner\Release\vcruntime140_1.dll
 
 @ECHO =============================
 @ECHO   systemtray build finished
